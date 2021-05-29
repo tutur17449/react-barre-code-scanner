ea037d6cc9c4339593d2443e77dd0eec88fe6867
Arthur <arthur.hamel1@orange.fr>
2021-05-29 12:52:26 +0200

    add missing dep in useeffect

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -129,7 +129,7 @@ export default function Scanner({ barreCode, setBarreCode }) {
       Quagga.offProcessed();
       Quagga.offDetected();
     };
-  }, []);
+  }, [currentDevice]);
 
   return (
     <>
